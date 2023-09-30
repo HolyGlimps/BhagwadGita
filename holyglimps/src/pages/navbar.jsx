51f0ebf2bd016eac1624769e0bfdc87ebeda1eea
Gopal Sharma <gopalsharmapmsg@gmail.com>
2023-09-30 23:07:47 +0530

    Colors enhanced

--- holyglimps/src/pages/navbar.jsx
+++ holyglimps/src/pages/navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="bg-cyan-400 p-4">
+    <nav className="bg-gradient-to-r from-[#fff9a8] to-[#eb863d] p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-2xl font-semibold">
           HolyGlimps
@@ -13,9 +13,9 @@ function Navbar() {
 
           <div className="group inline-block text-current hover:text-cyan-50 relative">
             <button> Account </button>
-            <ul className="absolute hidden group-hover:block bg-slate-100 text-gray-800 rounded-lg">
-              <li><Link to="/login" className="block px-4 py-2 hover:bg-gray-200">Login</Link></li>
-              <li><Link to="/signup" className="block px-4 py-2 hover:bg-gray-200">Signup</Link></li>
+            <ul className="absolute hidden group-hover:block bg-[#eb863d] text-gray-800 rounded-lg">
+              <li><Link to="/login" className="block px-4 py-2 hover:bg-[#fff9a8]">Login</Link></li>
+              <li><Link to="/signup" className="block px-4 py-2 hover:bg-[#fff9a8]">Signup</Link></li>
             </ul>
           </div>
         </div>
