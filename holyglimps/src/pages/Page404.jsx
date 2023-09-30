c04018c13d8d910f76697dcd98da7d270a6ab143
Neeraj <mrneeraaj@gmail.com>
2023-09-30 23:08:36 +0530

    update: page404 component naming convention changed

--- holyglimps/src/pages/Page404.jsx
+++ holyglimps/src/pages/Page404.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const page404 = () => {
+const Page404 = () => {
   return (
     <div class="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
       <div class="bg-white border border-gray-200 flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
@@ -37,4 +37,4 @@ const page404 = () => {
   );
 };
 
-export default page404;
+export default Page404;
