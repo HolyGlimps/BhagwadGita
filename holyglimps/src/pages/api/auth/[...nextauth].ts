fbb99cbf0adcdaeeab8a4fd575f095a0dab6140b
imvipindev <vipinchaudhary1809@gmail.com>
2023-10-11 12:57:29 +0530

    feat: google auth

--- holyglimps/src/pages/api/auth/[...nextauth].ts
+++ holyglimps/src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,29 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        
-        }),
-        
-        // ...add more providers here
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
     ],
-    // callbacks:
+
+
 }
 
 export default NextAuth(authOptions)
+
+
+// export const authOptions = {
+//     providers: [
+//      GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//      }),
+//     ],
+//     session: {
+//      strategy: 'jwt',
+//     },
+//    };
--- holyglimps/src/pages/index.tsx
+++ holyglimps/src/pages/index.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Homepage from './Home'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    // <main
+    // className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    // >
+    <Homepage />
 
-      <p>Hello world Test </p>
-
-    </main>
+    // </main>
   )
 }
