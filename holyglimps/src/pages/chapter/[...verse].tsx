784e08471c17ea357a823f80c7313dd1bb12eb82
nrj-kmr <mrneeraaj@gmail.com>
2023-10-17 18:37:21 +0530

    feat: :zap: render: shlokas are now rendered at the page
    now one can read the translations and commentries of a verse by clicking start reading at the summary page

--- holyglimps/src/pages/chapter/[...verse].tsx
+++ holyglimps/src/pages/chapter/[...verse].tsx
@@ -74,37 +74,48 @@ function VerseComponent({ verse }) {
   } = verse;
 
   return (
-    <div className="verse">
-      <h3 className='font-semibold'>
-        Chapter {chapter_number}, Verse {verse_number}
-      </h3>
-      <p>{text}</p>
-      
-      <h4 className='font-semibold'>Trans-literation</h4>
-      <p>{transliteration}</p>
+    <>
+      <nav>
+        {/* To Navigate user back and allow them to switch the chapter directly while he is reading a verse */}
+      </nav>
 
-      <h4 className='font-semibold'>Commentaries</h4>
-      {/* <ul>
-        {commentaries.map((commentary) => (
-          <li key={commentary.id}>
-            <strong>{commentary.author_name}</strong>: {commentary.description}
-          </li>
-        ))}
-      </ul> */}
+      <div className="verse">
+        <h3 className='font-extrabold text-xl'>
+          Chapter {chapter_number}, Verse {verse_number}
+        </h3>
+        <p className='ml-5 text-2xl'>{text}</p>
+        
+        {/* <h4 className='font-semibold'>Trans-literation</h4> */}
+        <p className='ml-5 text-lime-600'>{transliteration}</p>
 
-      <h4 className='font-semibold'>Translations</h4>
-      {/* <ul>
-        {translations.map((translation) => (
-          <li key={translation.id}>
-            <strong>{translation.author_name}</strong> ({translation.language}):{' '}
-            {translation.description}
-          </li>
-        ))}
-      </ul> */}
+        <div className='border border-gray-300 rounded-md m-2'>
+          <h4 className='font-bold text-xl pt-2 ml-3'>Translations</h4>
+          <ul className='ml-5'>
+            {translations && translations.map((translation) => (
+              <li key={translation.id} className='p-1'>
+                <strong className='text-orange-600'>{translation.author_name}</strong> ({translation.language}):{' '}
+                {translation.description}
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className='flex items-center justify-center'>
-        <button className='bg-blue border border-gray-500 rounded-md px-1'> Next </button>
+        <div className='border border-gray-300 rounded-md m-2'>
+          <h3 className='font-bold text-xl pt-1 ml-3'>Commentaries</h3>
+          <ul className='ml-5'>
+            {commentaries && commentaries.map((commentary) => (
+              <li className="p-1" key={commentary.id}>
+                <strong className='text-orange-600'>{commentary.author_name}</strong>: {commentary.description}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='flex items-center justify-center'>
+          <button className='bg-blue border border-gray-500 rounded-md px-1 mr-1'> Previous </button>
+          <button className='bg-blue border border-gray-500 rounded-md px-1 ml-1'> Next </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
